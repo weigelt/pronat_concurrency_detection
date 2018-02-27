3601d35505dddd026ab1fe3b912cd221ef552f86
Sebastian Weigelt <weigelt@kit.edu>
2018-02-27 17:24:11 +0100

    begun grammar filter

---
+++ src/main/java/edu/kit/ipd/parse/concurrency/data/ConcurrentAction.java
@@ -0,0 +1,11 @@
+package edu.kit.ipd.parse.concurrency.data;
+
+import java.util.List;
+
+import edu.kit.ipd.parse.luna.graph.INode;
+
+public class ConcurrentAction {
+	private Keyphrase keyphrase;
+	private List<INode> dependentPhrases;
+
+}
---
+++ src/main/java/edu/kit/ipd/parse/concurrency/filter/GrammarFilter.java
@@ -0,0 +1,18 @@
+package edu.kit.ipd.parse.concurrency.filter;
+
+import java.util.List;
+
+import edu.kit.ipd.parse.concurrency.data.ConcurrentAction;
+import edu.kit.ipd.parse.concurrency.data.Keyphrase;
+
+public class GrammarFilter {
+
+	public GrammarFilter() {
+		//TODO: constructor
+	}
+
+	public List<ConcurrentAction> filter(List<Keyphrase> keyphrases) {
+		//TODO: implement me!
+		return null;
+	}
+}

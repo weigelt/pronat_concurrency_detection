ddc942a7155fefe951689a0742c7f3ae84cba781
Sebastian Weigelt <weigelt@kit.edu>
2018-02-28 16:26:05 +0100

    fixed concurrent action representation

--- src/main/java/edu/kit/ipd/parse/concurrency/data/ConcurrentAction.java
+++ src/main/java/edu/kit/ipd/parse/concurrency/data/ConcurrentAction.java
@@ -8,4 +8,41 @@ public class ConcurrentAction {
 	private Keyphrase keyphrase;
 	private List<INode> dependentPhrases;
 
+	/**
+	 * @return the keyphrase
+	 */
+	public Keyphrase getKeyphrase() {
+		return keyphrase;
+	}
+
+	/**
+	 * @param keyphrase
+	 *            the keyphrase to set
+	 */
+	public void setKeyphrase(Keyphrase keyphrase) {
+		this.keyphrase = keyphrase;
+	}
+
+	/**
+	 * @return the dependentPhrases
+	 */
+	public List<INode> getDependentPhrases() {
+		return dependentPhrases;
+	}
+
+	/**
+	 * @param dependentPhrases
+	 *            the dependentPhrases to set
+	 */
+	public void setDependentPhrases(List<INode> dependentPhrases) {
+		this.dependentPhrases = dependentPhrases;
+	}
+
+	/**
+	 * @param dependentPhrase
+	 *            adds a dependent phrase to the list of dep. phrases
+	 */
+	public void addDependentPhrase(INode dependentPhrase) {
+		dependentPhrases.add(dependentPhrase);
+	}
 }

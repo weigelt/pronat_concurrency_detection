c444a1cb491588c05574e975fad0f292ad932339
Tobias Hey <hey@kit.edu>
2018-03-01 17:47:41 +0100

    refactoring SpecializedGrammarFilters

--- src/main/java/edu/kit/ipd/parse/concurrency/filter/OpeningGrammarFilter.java
+++ src/main/java/edu/kit/ipd/parse/concurrency/filter/OpeningGrammarFilter.java
@@ -14,8 +14,8 @@ public class OpeningGrammarFilter implements ISpecializedGrammarFilter {
 		INode[] rightActions = new INode[3];
 		rightActions[0] = keyphrase.getAttachedNodes().get(keyphrase.getAttachedNodes().size() - 1);
 
-		findActionNodes(leftActions, true);
-		findActionNodes(rightActions, false);
+		GrammarFilter.findActionNodes(leftActions, true);
+		GrammarFilter.findActionNodes(rightActions, false);
 
 		INode firstLeftAction = leftActions[1];
 		INode firstRightAction = rightActions[1];
@@ -27,27 +27,4 @@ public class OpeningGrammarFilter implements ISpecializedGrammarFilter {
 		return result;
 	}
 
-	private boolean findActionNodes(INode[] actions, boolean left) {
-		boolean foundAnd = false;
-		while ((left ? !actions[0].getIncomingArcsOfType(GrammarFilter.nextArcType).isEmpty()
-				: !actions[0].getOutgoingArcsOfType(GrammarFilter.nextArcType).isEmpty()) && actions[2] == null) {
-			actions[0] = left ? actions[0].getIncomingArcsOfType(GrammarFilter.nextArcType).get(0).getSourceNode()
-					: actions[0].getOutgoingArcsOfType(GrammarFilter.nextArcType).get(0).getTargetNode();
-			if (actions[0].getAttributeValue(GrammarFilter.ATTRIBUTE_NAME_ROLE) != null
-					&& actions[0].getAttributeValue(GrammarFilter.ATTRIBUTE_NAME_ROLE).toString()
-							.equalsIgnoreCase(GrammarFilter.ATTRIBUTE_VALUE_PREDICATE)) {
-				if (actions[1] == null) {
-					actions[1] = actions[0];
-				} else {
-					actions[2] = actions[0];
-				}
-			}
-			if (actions[1] != null) {
-				if (actions[0].getAttributeValue(GrammarFilter.ATTRIBUTE_NAME_VALUE).toString().equalsIgnoreCase(GrammarFilter.WORD_AND)) {
-					foundAnd = true;
-				}
-			}
-		}
-		return foundAnd;
-	}
 }

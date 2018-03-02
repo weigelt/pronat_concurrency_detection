148fdf78eebf93fac6bf25737889bae3fc8cf7ad
Tobias Hey <hey@kit.edu>
2018-03-02 15:27:12 +0100

    added more tests for KeyphraseFilter

--- src/test/java/edu/kit/ipd/parse/concurrency/filter/KeyphraseFilterTest.java
+++ src/test/java/edu/kit/ipd/parse/concurrency/filter/KeyphraseFilterTest.java
@@ -106,4 +106,50 @@ public class KeyphraseFilterTest {
 			}
 		}
 	}
+
+	@Test
+	public void testFilterKeywordAmbiguous1() {
+		String input = "the dog jumps and while the horse runs the dog barks";
+		List<INode> inputNodeList = new ArrayList<>();
+		String[] splitted = input.split(" ");
+		for (String string : splitted) {
+			INode currNode = pg.createNode(nodeType);
+			currNode.setAttributeValue("value", string);
+			inputNodeList.add(currNode);
+		}
+		String[] expected = new String[] { "and", "while" };
+		String expectedType = "OPENING";
+		List<Keyphrase> result = new KeyphraseFilter().filter(inputNodeList);
+		for (Keyphrase keyphrase : result) {
+			int i = 0;
+			Assert.assertEquals(expectedType, keyphrase.getPrimaryType().name());
+			for (INode node : keyphrase.getAttachedNodes()) {
+				Assert.assertEquals(expected[i], node.getAttributeValue("value").toString());
+				i++;
+			}
+		}
+	}
+
+	@Test
+	public void testFilterKeywordAmbiguous2() {
+		String input = "the dog jumps while the horse runs the dog barks";
+		List<INode> inputNodeList = new ArrayList<>();
+		String[] splitted = input.split(" ");
+		for (String string : splitted) {
+			INode currNode = pg.createNode(nodeType);
+			currNode.setAttributeValue("value", string);
+			inputNodeList.add(currNode);
+		}
+		String[] expected = new String[] { "while" };
+		String expectedType = "SEPARATING";
+		List<Keyphrase> result = new KeyphraseFilter().filter(inputNodeList);
+		for (Keyphrase keyphrase : result) {
+			int i = 0;
+			Assert.assertEquals(expectedType, keyphrase.getPrimaryType().name());
+			for (INode node : keyphrase.getAttachedNodes()) {
+				Assert.assertEquals(expected[i], node.getAttributeValue("value").toString());
+				i++;
+			}
+		}
+	}
 }

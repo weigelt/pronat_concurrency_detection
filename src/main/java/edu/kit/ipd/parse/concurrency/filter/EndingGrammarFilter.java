03769aaec4b144f67c99f9b8b9e0900139728674
Tobias Hey <hey@kit.edu>
2018-03-02 13:39:50 +0100

    implemented OpeningGrammarFilter and EndingGrammarFilter

--- src/main/java/edu/kit/ipd/parse/concurrency/filter/EndingGrammarFilter.java
+++ src/main/java/edu/kit/ipd/parse/concurrency/filter/EndingGrammarFilter.java
@@ -5,34 +5,25 @@ import edu.kit.ipd.parse.concurrency.data.Keyphrase;
 import edu.kit.ipd.parse.luna.data.MissingDataException;
 import edu.kit.ipd.parse.luna.graph.INode;
 
-public class WrappingGrammarFilter implements ISpecializedGrammarFilter {
+public class EndingGrammarFilter implements ISpecializedGrammarFilter {
 
 	@Override
 	public ConcurrentAction filter(Keyphrase keyphrase) throws MissingDataException {
-
 		INode[] leftActions = new INode[3];
 		leftActions[0] = keyphrase.getAttachedNodes().get(0);
-		INode[] rightActions = new INode[3];
-		rightActions[0] = keyphrase.getAttachedNodes().get(keyphrase.getAttachedNodes().size() - 1);
 
 		boolean leftAnd = GrammarFilter.findActionNodes(leftActions, true);
-		boolean rightAnd = GrammarFilter.findActionNodes(rightActions, false);
 
 		INode firstLeftAction = leftActions[1];
 		INode secondLeftAction = leftActions[2];
-		INode firstRightAction = rightActions[1];
-		INode secondRightAction = rightActions[2];
 
 		ConcurrentAction result = null;
-		if (firstRightAction != null && secondRightAction != null && rightAnd) {
-			result = DependentNodesExtractor.extract(keyphrase, firstRightAction, secondRightAction, false);
-		} else if (firstLeftAction != null && secondLeftAction != null && leftAnd) {
+		if (firstLeftAction != null && secondLeftAction != null && leftAnd) {
 			result = DependentNodesExtractor.extract(keyphrase, secondLeftAction, firstLeftAction, true);
-		} else if (firstRightAction != null && secondRightAction != null) {
-			result = DependentNodesExtractor.extract(keyphrase, firstRightAction, secondRightAction, false);
 		} else {
 			//TODO: what now?
 		}
 		return result;
 	}
+
 }
--- src/main/java/edu/kit/ipd/parse/concurrency/filter/OpeningGrammarFilter.java
+++ src/main/java/edu/kit/ipd/parse/concurrency/filter/OpeningGrammarFilter.java
@@ -9,21 +9,21 @@ public class OpeningGrammarFilter implements ISpecializedGrammarFilter {
 
 	@Override
 	public ConcurrentAction filter(Keyphrase keyphrase) throws MissingDataException {
-		INode[] leftActions = new INode[3];
-		leftActions[0] = keyphrase.getAttachedNodes().get(0);
 		INode[] rightActions = new INode[3];
 		rightActions[0] = keyphrase.getAttachedNodes().get(keyphrase.getAttachedNodes().size() - 1);
 
-		GrammarFilter.findActionNodes(leftActions, true);
-		GrammarFilter.findActionNodes(rightActions, false);
+		boolean rightAnd = GrammarFilter.findActionNodes(rightActions, false);
 
-		INode firstLeftAction = leftActions[1];
 		INode firstRightAction = rightActions[1];
+		INode secondRightAction = rightActions[2];
 
-		if (firstLeftAction != null && firstRightAction != null) {
-			return SeparatingDependentNodesExtractor.extract(keyphrase, firstLeftAction, firstRightAction);
+		ConcurrentAction result = null;
+		if (firstRightAction != null && secondRightAction != null && rightAnd) {
+			result = DependentNodesExtractor.extract(keyphrase, firstRightAction, secondRightAction, false);
+		} else {
+			//TODO: what now?
 		}
-		return null;
+		return result;
 	}
 
 }

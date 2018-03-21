1e73c603b454aad6fdede4b67b4fb7131a4767a9
Tobias Hey <hey@kit.edu>
2018-03-21 11:38:12 +0100

    added and improved tests

--- src/test/java/edu/kit/ipd/parse/concurrency/ConcurrencyCorefTest.java
+++ src/test/java/edu/kit/ipd/parse/concurrency/ConcurrencyCorefTest.java
@@ -1,5 +1,6 @@
 package edu.kit.ipd.parse.concurrency;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
@@ -138,7 +139,7 @@ public class ConcurrencyCorefTest {
 	public void endingCorefTest() {
 		ppd = new PrePipelineData();
 		//@formatter:off
-		String input = "Bob jumps and Alice looks at the table runs to it and jumps on it at the same time";
+		String input = "Bob jumps and Alice looks at the table and she runs to it and jumps on it at the same time";
 		//@formatter:on
 		ppd.setMainHypothesis(StringToHypothesis.stringToMainHypothesis(input, true));
 
@@ -154,7 +155,7 @@ public class ConcurrencyCorefTest {
 			Assert.assertEquals(expected[i], node.getAttributeValue("value").toString());
 			i++;
 		}
-		int[] expectedSpan = new int[] { 3, 14 };
+		int[] expectedSpan = new int[] { 3, 16 };
 		Assert.assertEquals(expectedSpan[0], action.getDependentPhrases().get(0).getAttributeValue("position"));
 		Assert.assertEquals(expectedSpan[1],
 				action.getDependentPhrases().get(action.getDependentPhrases().size() - 1).getAttributeValue("position"));
@@ -211,6 +212,51 @@ public class ConcurrencyCorefTest {
 		int[] expectedSpan = new int[] { 0, 13 };
 		Assert.assertEquals(expectedSpan[0], action.getDependentPhrases().get(0).getAttributeValue("position"));
 		Assert.assertEquals(expectedSpan[1],
+				action.getDependentPhrases().get(action.getDependentPhrases().size() - 1).getAttributeValue("position"));
+
+	}
+
+	@Test
+	public void separatingCorefTestImperative() {
+		ppd = new PrePipelineData();
+		//@formatter:off
+		String input = "the dog runs to the table and jumps on it and in the meantime Alice looks at the recipe and takes it";
+		//@formatter:on
+		ppd.setMainHypothesis(StringToHypothesis.stringToMainHypothesis(input, true));
+
+		IGraph graph = executePreviousStages(ppd);
+		concAgent.setGraph(graph);
+		concAgent.exec();
+		List<ConcurrentAction> actions = concAgent.getConcurrentActions();
+		Assert.assertEquals(1, actions.size());
+		ConcurrentAction action = actions.get(0);
+		String[] expected = new String[] { "in", "the", "meantime" };
+		int i = 0;
+		for (INode node : action.getKeyphrase().getAttachedNodes()) {
+			Assert.assertEquals(expected[i], node.getAttributeValue("value").toString());
+			i++;
+		}
+		int[] expectedSpanBefore = new int[] { 0, 9 };
+		int[] expectedSpanAfter = new int[] { 14, 21 };
+		int lastBefore = 0;
+		int index = 0;
+		Assert.assertEquals("Before span does not start where expected", expectedSpanBefore[0],
+				action.getDependentPhrases().get(0).getAttributeValue("position"));
+		for (INode node : action.getDependentPhrases()) {
+			int nodePosition = (int) node.getAttributeValue("position");
+			boolean isInsideSpan = expectedSpanBefore[0] <= nodePosition && nodePosition <= expectedSpanBefore[1];
+			if (lastBefore == 0 && isInsideSpan == false) {
+				Assert.assertEquals("Before span does not end where expected", expectedSpanBefore[1],
+						action.getDependentPhrases().get(index - 1).getAttributeValue("position"));
+				lastBefore = index - 1;
+				Assert.assertEquals("After span does start not where expected", expectedSpanAfter[0], nodePosition);
+			}
+			isInsideSpan = isInsideSpan || expectedSpanAfter[0] <= nodePosition && nodePosition <= expectedSpanAfter[1];
+			Assert.assertTrue("Dependent Node at position " + nodePosition + " is not inside expected spans: "
+					+ Arrays.toString(expectedSpanBefore) + ", " + Arrays.toString(expectedSpanAfter), isInsideSpan);
+			index++;
+		}
+		Assert.assertEquals("After span does not end where expected", expectedSpanAfter[1],
 				action.getDependentPhrases().get(action.getDependentPhrases().size() - 1).getAttributeValue("position"));
 
 	}
